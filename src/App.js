96d03cca871c07831b16306bbd989062dd33becc
Thuraya-H <Thuraya.hamed1@gmail.com>
2020-11-25 12:15:51 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import PersonCard from './components/PersonCard'
+
+function App() {
+  return (
+    <div className="App">
+        <PersonCard first_name={"Jane"} last_name={"Deo"} age={45} hair_color={"Black"} />
+        <PersonCard first_name={"John"} last_name={"Smith"} age={88} hair_color={"Brown"} />
+        <PersonCard first_name={"Millard"} last_name={"Fillmore"} age={50} hair_color={"Brown"} />
+        <PersonCard first_name={"Maria"} last_name={"Smith"} age={62} hair_color={"Brown"} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PersonCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+    
+    
+class PersonCard extends Component {
+    render() {
+    var {first_name, last_name, age, hair_color} = this.props;
+        return <div>
+    <h1>{last_name}, {first_name}</h1>
+    <p>Age: {age}</p>
+    <p>Hair Color: {hair_color}</p>
+    </div>;
+    }
+}
+    
+export default PersonCard;
